84ef0c7c54498fb61b7509e68f3904bd206c6a9c
Pavel Setsko <Pashech555@gmail.com>
2022-07-21 16:10:02 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,24 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for(int i = 1; i <= cathetusLength; i++){
+            for(int j = cathetusLength; j > i; j--){
+                System.out.print(" ");
+            }
 
+            for(int k = i; k > 0; k--){
+                System.out.print(k);
+            }
+
+            for (int k = 2; k <= i; k++){
+                System.out.print(k);
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(9);
     }
 }
